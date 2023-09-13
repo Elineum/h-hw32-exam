9d67dd8036bf74b240eb2e5fd44b08f94b187118
Elineum <you@example.com>
2023-09-14 01:42:31 +0300

    added Reviews components with mock data rn

--- src/components/Reviews/Reviews.tsx
+++ src/components/Reviews/Reviews.tsx
@@ -1,5 +1,56 @@
+import { ReviewsSlideItem } from "./ReviewsSlideItem";
+import { Pagination, Autoplay } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
 import "./Reviews.scss";
+import "swiper/scss";
+import "swiper/scss/pagination";
+
+const userReviewList = [
+  {
+    userPhoto: "",
+    userMessage:
+      "I’m very grateful to you for introducing me and my whole family to your extensive range of smoothies and organic juices. We are true fans and regular customers of your store!",
+    userFullname: "PATRIC GOORMAN",
+    userStatus: "Client",
+  },
+  {
+    userPhoto: "",
+    userMessage:
+      "I am amazed and extremely satisfied by your smoothies, their taste, and the customer service you provide to unexperienced clients who know nothing about organic drinks.",
+    userFullname: "KATE SMITH",
+    userStatus: "Client",
+  },
+  {
+    userPhoto: "",
+    userMessage:
+      "Your healthy drinks have a wonderful taste and the amazing selection of their flavors is what really attracts me in your store. Thank you for fast delivery and your fresh drinks!",
+    userFullname: "SAM WILSON",
+    userStatus: "Client",
+  },
+];
 
 export const Reviews: React.FC = () => {
-  return <section className="reviews">Reviews</section>;
+  return (
+    <section className="reviews">
+      <div className="container">
+        <h2 className="reviews__title">What People Say</h2>
+        <Swiper
+          modules={[Pagination, Autoplay]}
+          pagination={{ clickable: true }}
+          className="reviews__swiper"
+          autoplay={{
+            delay: 5000,
+            disableOnInteraction: false,
+          }}
+          slidesPerView={1}
+        >
+          {userReviewList.map((sliderList, index) => (
+            <SwiperSlide>
+              <ReviewsSlideItem {...sliderList} key={index} />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
+    </section>
+  );
 };
